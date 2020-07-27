a96ccf92dafdc1de1d3dd385a9e1ed38fe50434c
Aldi Ramadhan <67244580+aldirmdn@users.noreply.github.com>
2020-07-28 06:23:14 +0700

    Delete set-image.js

--- set-image.js
+++
@@ -1,4 +0,0 @@
-$('img').on('click', function(e) {
-  $('#imgViewer').html('').append( $(e.currentTarget).clone().removeClass('img-responsive').removeClass('img-thumbnail') )
-  $('#viewImg').modal('show')
-})
